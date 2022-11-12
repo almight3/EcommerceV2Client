7f340848537496e434a02e272155c40d393ada69
Priyanshu Shrivastava <shrivastavap136@gmail.com>
2022-11-12 12:22:31 +0530

    style - changed card style

--- src/Component/ProductCard/ProductCard.jsx
+++ src/Component/ProductCard/ProductCard.jsx
@@ -2,25 +2,28 @@ import {useState} from 'react';
 import {Link} from "react-router-dom";
 import { Rating } from '@smastrom/react-rating';
 import '@smastrom/react-rating/style.css';
-
+import {FaRegHeart} from "react-icons/fa";
+import {FaHeart} from  "react-icons/fa";
 
 function ProductCard({product}) {
   const [rating, setRating] = useState(3);
   return (
-    <Link to= "/product/633d84f3aea27e5cfa76c833" >
-        <div className=" h-80 w-56	bg-white shadow-xl m-4">
+   
+        <div className=" h-80 w-56	bg-white shadow-xl m-4 relative">
         <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" 	className='h-48 text-lg'/></figure>
-        <p className='text-l pt-1 px-3 decoration-gray-400 font-light'>Solid Hooded Sweatshirt</p>
-        <p className='text-l px-4 pb-1 font-bold'>Rs. 500</p>
-        <div className="rating rating-sm px-4">
-        <Rating style={{ maxWidth: 120 }} value={4} readOnly={true} />       
+        <p className='text-l px-3 decoration-gray-400 font-light text-center'>Solid Hooded Sweatshirt</p>
+        <p className='text-l px-4 font-bold text-center'>Rs. 500</p>
+        <div className="rating rating-sm px-4 flex justify-center">
+        <Rating style={{ maxWidth: 100 }} value={4} readOnly={true} />       
         </div>
-       <ul className='px-3 pt-1'>
-           <li className="badge badge-outline mx-1">Clothing</li>
-           <li  className="badge badge-outline mx-1">Mens</li> 
-       </ul>
+        <div className=' absolute top-0 h-full w-full border flex items-end  opacity-0 hover:opacity-100'>
+          <div className='flex justify-center items-center w-full border cursor-pointer'>
+            <FaRegHeart color='#ef4444' size={22} className='w-4/12 mx-auto'/>
+            <button className='p-3  font-medium  bg-black text-white w-8/12	'>Add To Cart</button>
+          </div>
         </div>
-    </Link>
+        </div>
+    
   )
 }
 
