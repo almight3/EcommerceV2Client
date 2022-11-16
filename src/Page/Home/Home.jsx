d6406528970bad6708d1c4f927df70bd6c60e35c
Priyanshu Shrivastava <shrivastavap136@gmail.com>
2022-11-16 23:26:16 +0530

    changed design of landing page

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,96 +1,70 @@
 import React from 'react';
 import "./home.css";
-import EcomBanner from "../../image/EcommerceBanner.jpg"
-import MensTshirt from "../../image/mens-tshirt.jpg";
-import MensSneaker from "../../image/mens-sneaker.jpg";
-import MensDenim from "../../image/mens-denim.jpg";
-import MensShirts from "../../image/mens-shirts.jpg";
-
+import Banner from "../../image/banner/banner.jpg";
+import OnePiece from "../../image/anime/onepiece.jpg";
+import Naruto from "../../image/anime/naruto.png" ;
+import DragonBall from "../../image/anime/dragonball.jpg";
+import Tshirt from "../../image/ProdcutCategory/LuffyZoroTShirt.png";
+import Poster from "../../image/ProdcutCategory/poster.jpg";
+import Manga from "../../image/ProdcutCategory/manga.jpg";
 function Home() {
   return (
     <div className='mx-auto	w-10/12	'> 
-          <div className='mx-auto my-5'>
-            <img src={EcomBanner} alt="wesite banner" className='w-full' />
+          <div className='relative mx-auto h-96 	my-5'>
+            <img src={Banner} alt="website banner" className='w-full h-full object-cover' />
+            <div className='absolute top-0 h-full w-full flex items-center justify-center  bg-black/20  opacity-0 hover:opacity-100 transition-all	 duration-300 ease-in-out'>
+            <button className='px-5 py-2 bg-black/60 text-white border-2 border-white'>All Product</button>
+            </div>
           </div>
-          <div className='flex flex-wrap justify-around w-full'>
-              <h1 className='w-11/12 text-2xl font-medium'>Explore Mens Clothing</h1>
+          <div className='flex flex-wrap justify-center w-full'>
+              <h1 className='w-full text-center text-2xl font-medium'>Anime</h1>
               <div className='relative h-72	w-80 m-5 overflow-hidden shadow	' >
-                  <img src={MensTshirt} alt="mens tshirt" className='h-full w-full' />
-                  <div className='absolute top-0 h-full w-full flex items-center justify-center  bg-black/40  opacity-0 hover:opacity-100 ttransition-colors	 duration-300	'>
-                     <button className='px-5 py-2 bg-black text-white rounded'>T Shirt</button>
+                  <img src={OnePiece} alt="" className='h-full w-full object-cover' />
+                  <div className='absolute top-0 h-full w-full flex items-center justify-center  bg-black/30  opacity-0 hover:opacity-100 transition-all duration-300 ease-in-out'>
+                     <button className='px-5 py-2 bg-black/60 text-white border-2 border-white'>One Piece</button>
                   </div>
               </div>
               <div className='relative h-72	w-80 m-5 overflow-hidden shadow	' >
-                  <img src={MensSneaker} alt="mens tshirt" className='h-full w-full' />
-                  <div className='absolute top-0 h-full w-full flex items-center justify-center  bg-black/40  opacity-0 hover:opacity-100 ttransition-colors	 duration-300	'>
-                     <button className='px-5 py-2 bg-black text-white rounded'>Shoes</button>
+                  <img src={Naruto} alt="" className='h-full w-full object-cover' />
+                  <div className='absolute top-0 h-full w-full flex items-center justify-center  bg-black/30  opacity-0 hover:opacity-100 transition-all	 duration-300 ease-in-out	'>
+                     <button className='px-5 py-2 bg-black/60 text-white border-2 border-white'>Naruto</button>
                   </div>
               </div>
               <div className='relative h-72	w-80 m-5 overflow-hidden shadow	' >
-                  <img src={MensDenim} alt="mens tshirt" className='h-full w-full' />
-                  <div className='absolute top-0 h-full w-full flex items-center justify-center  bg-black/40  opacity-0 hover:opacity-100 ttransition-colors	 duration-300	'>
-                     <button className='px-5 py-2 bg-black text-white rounded'>Jeans</button>
+                  <img src={DragonBall} alt="" className='h-full w-full object-cover' />
+                  <div className='absolute top-0 h-full w-full flex items-center justify-center  bg-black/30  opacity-0 hover:opacity-100 transition-all	 duration-300 ease-in-out	'>
+                     <button className='px-5 py-2 bg-black/60 text-white border-2 border-white'>Dragon Ball</button>
                   </div>
               </div>
-              <div className='relative h-72	w-80 m-5 overflow-hidden shadow	' >
-                  <img src={MensShirts} alt="mens tshirt" className='h-full w-full' />
-                  <div className='absolute top-0 h-full w-full flex items-center justify-center  bg-black/40  opacity-0 hover:opacity-100 ttransition-colors	 duration-300	'>
-                     <button className='px-5 py-2 bg-black text-white rounded'>Shirt</button>
+             </div>    
+          <div className='flex flex-wrap justify-center w-full'>
+              <h1 className='w-full text-center text-2xl font-medium'>Explore Category </h1>
+              <div>
+                 <div className='relative h-72	w-72 m-5 mb-0 overflow-hidden shadow	' >
+                  <img src={Tshirt} alt="" className='h-full w-full object-cover' />
+                  <div className='absolute top-0 h-full w-full flex items-center justify-center  bg-black/40  opacity-0 hover:opacity-100 transition-all duration-300 ease-in-out	'>
+                     <button className='px-5 py-2 bg-black/60 text-white border-2 border-white'>View</button>
                   </div>
               </div>
-          </div>
-          <div className='flex flex-wrap justify-around w-full'>
-              <h1 className='w-11/12 text-2xl font-medium'>Explore Womens Clothing</h1>
-              <div className='relative h-72	w-80 m-5 overflow-hidden shadow	' >
-                  <img src={MensTshirt} alt="mens tshirt" className='h-full w-full' />
-                  <div className='absolute top-0 h-full w-full flex items-center justify-center  bg-black/40  opacity-0 hover:opacity-100 ttransition-colors	 duration-300	'>
-                     <button className='px-5 py-2 bg-black text-white rounded'>T Shirt</button>
+              <p className='text-center m-1 text-xl font-bold'>T Shirt</p>
+              </div>
+              <div>
+                <div className='relative h-72 w-72 m-5 mb-0 overflow-hidden shadow	' >
+                  <img src={Poster} alt="" className='h-full w-full object-cover' />
+                  <div className='absolute top-0 h-full w-full flex items-center justify-center  bg-black/40  opacity-0 hover:opacity-100 transition-all duration-300 ease-in-out	'>
+                     <button className='px-5 py-2 bg-black/60 text-white border-2 border-white'>View</button>
+                  </div> 
+              </div>
+              <p className='text-center m-1 text-xl font-bold'>Anime Poster</p>
+              </div>
+              <div>
+              <div className='relative h-72	w-72 m-5 mb-0 overflow-hidden shadow	' >
+                  <img src={Manga} alt="" className='h-full w-full object-cover' />
+                  <div className='absolute top-0 h-full w-full flex items-center justify-center  bg-black/40  opacity-0 hover:opacity-100 transition-all duration-300 ease-in-out	'>
+                     <button className='px-5 py-2 bg-black/60 text-white border-2 border-white'>View</button>
                   </div>
               </div>
-              <div className='relative h-72	w-80 m-5 overflow-hidden shadow	' >
-                  <img src={MensSneaker} alt="mens tshirt" className='h-full w-full' />
-                  <div className='absolute top-0 h-full w-full flex items-center justify-center  bg-black/40  opacity-0 hover:opacity-100 ttransition-colors	 duration-300	'>
-                     <button className='px-5 py-2 bg-black text-white rounded'>Shoes</button>
-                  </div>
-              </div>
-              <div className='relative h-72	w-80 m-5 overflow-hidden shadow	' >
-                  <img src={MensDenim} alt="mens tshirt" className='h-full w-full' />
-                  <div className='absolute top-0 h-full w-full flex items-center justify-center  bg-black/40  opacity-0 hover:opacity-100 ttransition-colors	 duration-300	'>
-                     <button className='px-5 py-2 bg-black text-white rounded'>Jeans</button>
-                  </div>
-              </div>
-              <div className='relative h-72	w-80 m-5 overflow-hidden shadow	' >
-                  <img src={MensShirts} alt="mens tshirt" className='h-full w-full' />
-                  <div className='absolute top-0 h-full w-full flex items-center justify-center  bg-black/40  opacity-0 hover:opacity-100 ttransition-colors	 duration-300	'>
-                     <button className='px-5 py-2 bg-black text-white rounded'>Shirt</button>
-                  </div>
-              </div>
-          </div>
-          <div className='flex flex-wrap justify-around w-full'>
-              <h1 className='w-11/12 text-2xl font-medium'>Explore Accessories </h1>
-              <div className='relative h-72	w-80 m-5 overflow-hidden shadow	' >
-                  <img src={MensTshirt} alt="mens tshirt" className='h-full w-full' />
-                  <div className='absolute top-0 h-full w-full flex items-center justify-center  bg-black/40  opacity-0 hover:opacity-100 ttransition-colors	 duration-300	'>
-                     <button className='px-5 py-2 bg-black text-white rounded'>T Shirt</button>
-                  </div>
-              </div>
-              <div className='relative h-72	w-80 m-5 overflow-hidden shadow	' >
-                  <img src={MensSneaker} alt="mens tshirt" className='h-full w-full' />
-                  <div className='absolute top-0 h-full w-full flex items-center justify-center  bg-black/40  opacity-0 hover:opacity-100 ttransition-colors	 duration-300	'>
-                     <button className='px-5 py-2 bg-black text-white rounded'>Shoes</button>
-                  </div>
-              </div>
-              <div className='relative h-72	w-80 m-5 overflow-hidden shadow	' >
-                  <img src={MensDenim} alt="mens tshirt" className='h-full w-full' />
-                  <div className='absolute top-0 h-full w-full flex items-center justify-center  bg-black/40  opacity-0 hover:opacity-100 ttransition-colors	 duration-300	'>
-                     <button className='px-5 py-2 bg-black text-white rounded'>Jeans</button>
-                  </div>
-              </div>
-              <div className='relative h-72	w-80 m-5 overflow-hidden shadow	' >
-                  <img src={MensShirts} alt="mens tshirt" className='h-full w-full' />
-                  <div className='absolute top-0 h-full w-full flex items-center justify-center  bg-black/40  opacity-0 hover:opacity-100 ttransition-colors	 duration-300	'>
-                     <button className='px-5 py-2 bg-black text-white rounded'>Shirt</button>
-                  </div>
+              <p className='text-center m-1 text-xl font-bold'>Manga</p>
               </div>
           </div>
     </div>
