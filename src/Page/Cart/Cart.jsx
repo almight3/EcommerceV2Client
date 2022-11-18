d037652ce6f2f4dec9ef45f6b6e3de928c2f00f4
Priyanshu Shrivastava <shrivastavap136@gmail.com>
2022-11-18 21:04:20 +0530

    removed dummy data

--- src/Page/Cart/Cart.jsx
+++ src/Page/Cart/Cart.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import CartItem from '../../Component/CartItem/CartItem';
 import CartTotal from '../../Component/CartItem/CartTotal';
-import {useSelector,useDispatch} from "react-redux";
+import {useSelector} from "react-redux";
 
 function Cart() {
+  const {cartItems} = useSelector(state=>state.cart);
+  console.log(cartItems)
   return (
     <div className='flex justify-center w-9/12 mx-auto m-1 '>
        <div className='bg-white shadow-xl w-2/4	flex flex-col	m-2'>
-          <CartItem />
-          <CartItem />
-          <CartItem />
-          <CartItem />
+          {
+             cartItems.map((items)=>{
+              console.log(items)
+              return <CartItem items={items}/>
+            }) 
+            
+          }
        </div>
        <div className="w-80 h-72	 bg-white shadow-xl m-2">
           <CartTotal />
