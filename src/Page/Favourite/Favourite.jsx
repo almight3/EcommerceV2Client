a15291ef749db30ba87609bbf0131ca15b7077f3
Priyanshu Shrivastava <shrivastavap136@gmail.com>
2022-11-27 00:25:26 +0530

    refactor

--- src/Page/Favourite/Favourite.jsx
+++ src/Page/Favourite/Favourite.jsx
@@ -6,9 +6,9 @@ function Favourite() {
 
   return (
     <div className='w-8/12	mx-auto flex flex-wrap justify-center'>
-        <h1 className='w-10/12 mx-4 text-2xl font-semibold'>Favourite Products</h1>
+        <h1 className='w-10/12 mx-4 text-2xl font-semibold'>Products In Whislist ({favouriteProduct.length})</h1>
         {
-          favouriteProduct.map(fav=><FavouriteCard product={fav}/>)
+         favouriteProduct.length === 0 ? <h1 className='w-10/12 m-8 text-center text-2xl font-semibold'>Whislist is Empty</h1>:favouriteProduct.map(fav=><FavouriteCard product={fav}/>)
         }
     </div>
   )
